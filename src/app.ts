81784fba61ba0801dde5361a35b371988c21ee63
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-06-16 18:44:23 -0300

    Validações
    Crieu as validações dos produtos e testes que cubram as funcionalidades deste endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,18 @@ import express from 'express';
 import productController from './controller/productController';
 import procurarPedido from './controller/orderController';
 import loginController from './controller/login.controller';
-import loginMiddleware from './middleware/loginMiddleware';
+import loginMiddleware from './middlewares/loginMiddleware';
+import productMiddlewar from './middlewares/productMiddlewar';
 
 const app = express();
 
 app.use(express.json());
-app.post('/products', productController.cadastrarProduto);
+app.post(
+  '/products', 
+  productMiddlewar.validateName,
+  productMiddlewar.valitdatePrice, 
+  productController.cadastrarProduto,
+);
 app.get('/products', productController.procurarProduto);
 app.get('/orders', procurarPedido.procurarPedido);
 app.post('/login', loginMiddleware, loginController.logincontroller);
